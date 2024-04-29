2ac598ae4b39eeb553a0e0773acfc77a4124cb29
ALNosir <152771027+nosir123@users.noreply.github.com>
2024-04-29 15:51:48 +0500

    Add files via upload

---
+++ django-startproject.py
@@ -0,0 +1,9 @@
+#!C:\Users\Hp\Desktop\ish hozirga\venv\Scripts\python.exe
+
+def main():
+    from django_startproject.management import start_project
+    start_project()
+
+
+if __name__ == '__main__':
+    main()
